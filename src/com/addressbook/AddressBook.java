69bc31796b03b865abeaedc24b5fa8c75dc39d39
Dinesh130914 <dineshthiyagu99@gmail.com>
2022-01-07 01:08:41 +0530

    Get Number of contacts by using state

--- src/com/addressbook/AddressBook.java
+++ src/com/addressbook/AddressBook.java
@@ -22,11 +22,11 @@ public class AddressBook {
 
 			if (addressBook.isEmpty()) {
 
-				System.out.println("1. Add Contact" + "\n6. Exit");
+				System.out.println("1. Add Contact\n" + "6.Number Contacts\n7. Exit");
 			} else {
 
 				System.out.println("1. Add Contact"
-						+ "\n2. Display Contact\n3. Edit Contact\n4.Delete Contact\n5.Search(By City)\n 6.Exit");
+						+ "\n2. Display Contact\n3. Edit Contact\n4.Delete Contact\n5.Search(By City)\n6.Number Of Contacts\n 7.Exit");
 			}
 
 			String option = scanner.nextLine();
@@ -51,7 +51,17 @@ public class AddressBook {
 			case "5":
 				searchCity(scanner);
 				break;
+				
 			case "6":
+				
+				if (!addressBook.isEmpty()) {
+					numOfContacts(scanner);
+				} else {
+					System.out.println("No Contacts");
+				}
+				break;
+
+			case "7":
 				isExit = true;
 				showContacts();
 				break;
@@ -62,6 +72,15 @@ public class AddressBook {
 			}
 		}
 		scanner.close();
+	}
+
+	private static void numOfContacts(Scanner scanner) {
+
+		System.out.println("Number of contacts by using state");
+		String state = scanner.next();
+
+		long count = addressBook.stream().filter(contacts -> contacts.getState().equalsIgnoreCase(state)).count();
+		System.out.println("Number Of Contacts :"+count);
 	}
 
 	public static String validateFirstName(String firstName, Scanner scanner) {
